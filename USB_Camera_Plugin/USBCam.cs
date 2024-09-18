360b19ef855e5a3d06d5fb527d8f3102d833587a
ACooperDev <ac11588@yahoo.com>
2024-09-18 18:37:16 -0400

    Add project files.

---
+++ USB_Camera_Plugin/USBCam.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cognex.Designer.Core;
+using Emgu.CV;
+using Cognex.Designer.Components;
+using Cognex.Designer.Core;
+using Cognex.Designer.Core.Functions;
+using Cognex.Designer.Scripting;
+using Cognex.VisionPro;
+
+//Scriptable component for USB cameras based on EMGU.CV 4.9.0
+//https://github.com/emgucv/emgucv/releases/tag/4.9.0
+
+namespace USB_Camera_Plugin
+{
+    //The category in the Component Browser when a new component is created.
+    [Category("USB_Cameras")]
+    //The name displayed under the Category.
+    [DisplayName("USB Cameras")]
+    //The description of the component.
+    [Description("A component for capturing images from a USB camera.")]
+    //Expose a property to the new component dialog aka Parameter Configuration window.
+    [Parameter(1, nameof(_camIndex), "Camera Index", typeof(int), null)]
+    //The tpye ID used during save. Allows you to refactor the code later while not breaking serialization.
+    [TypeName("ScriptableComponent")]
+    public class USBCam : UserComponentBase
+    {
+        private readonly ScriptablePoint[] _scriptPoints;
+        private int _camIndex;
+
+        private const string ImageResultEventID = "ReadImageEvent";
+        private readonly ScriptablePoint _readImageEvent;
+
+        //Do I need this?
+        protected override ScriptablePoint[] GetScriptPoints()
+        {
+            return _scriptPoints;
+        }
+
+        //Events for the component.
+        public USBCam()
+        {
+            _readImageEvent = new ScriptablePoint(
+                ImageResultEventID,
+                "Read Image Event",
+                "Some description",
+                    runParameters: new[]
+                        {
+                        new ArgumentDescriptor("ImageResult", typeof(CogImage8Grey)),
+                        },
+                returnType: typeof(void));
+            //Create scriptable points array to make events accesable in Designer.
+            _scriptPoints = new[] {_readImageEvent};
+
+        }
+
+    }
+    
+}
